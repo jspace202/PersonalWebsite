e685ee33e400f78ed26263c41f60823357e50d13
Jaden Patel <jspace202@gmail.com>
2024-08-06 17:08:42 -0500

    Frontend Setup (React)
    "proxy": "http://localhost:5000" -> client/package.json (sets up proxy to backend server, allows React app to make API requests to backend without CORS issues)
    [Create folders 'components' and 'pages' in 'client/src']
    ExampleComponent.js (defines simple React component)
    ExamplePages.js (defines a react page that uses example component)

---
+++ client/src/pages/ExamplePages.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ExampleComponent from '../components/ExampleComponent';
+
+const ExamplePage = () => {
+  return (
+    <div>
+      <h1>Example Page</h1>
+      <ExampleComponent />
+    </div>
+  );
+};
+
+export default ExamplePage;
