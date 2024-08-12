101d95a028da4bffe04b82f3d7f7cd577687c695
Jaden Patel <jspace202@gmail.com>
2024-08-11 22:30:25 -0500

    part 2

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../assets/styles/main.scss'
+import { ReactComponent as HUDIcon} from '../assets/images/HUDicon.svg'
+import SocialIcons from './Socials/SocialIcons';
 
 const Footer = () => {
   return (
     <div className='Footer'>
-      <div className='ask'>
-        Footer
-      </div>
+      <HUDIcon className='FooterIcon'/>
+      <SocialIcons />
+      <text>Copyright © 2024 Jaden Patel</text>
     </div>
   );
 };
