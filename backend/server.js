8b4d46eb674f3c5ffef26baf93729d0246746c55
Jaden Patel <jspace202@gmail.com>
2024-08-17 00:39:09 -0500

    Get Access Token from Spotify

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,13 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 const axios = require('axios');
+const qs = require('qs');
 
 dotenv.config();
+
+const client_id = process.env.SPOTIFY_CLIENT_ID; // Your client id
+const client_secret = process.env.SPOTIFY_CLIENT_SECRET; // Your secret
+const auth_token = Buffer.from(`${client_id}:${client_secret}`, 'utf-8').toString('base64');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -45,6 +50,26 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/auth', async (req, res) => {
+  try{
+    //make post request to SPOTIFY API for access token, sending relavent info
+    const token_url = 'https://accounts.spotify.com/api/token';
+    const data = qs.stringify({'grant_type':'client_credentials'});
+
+    const response = await axios.post(token_url, data, {
+      headers: { 
+        'Authorization': `Basic ${auth_token}`,
+        'Content-Type': 'application/x-www-form-urlencoded' 
+      }
+    })
+    //return access token
+    // Send the access token in the response
+    res.json({ access_token: response.data.access_token }) 
+  }catch(error){
+    res.status(500).json({ error: 'Failed to fetch access token' });
+    console.log(error);
+  }
+});
 
 app.post('/api/upload', upload.single('file'), (req, res) => {
   res.json(req.file);
